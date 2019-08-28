005439b5bca8484386a30a38cc036fe46a83519f
congcong <15110253002@163.com>
2019-08-28 15:45:45 +0800

    add

--- project2/src/views/business/Home.vue
+++ project2/src/views/business/Home.vue
@@ -1,23 +1,19 @@
 <template>
   <!-- 首页 -->
   <div>
-    <div class="pro_container">
-      发动机和那个数据库
-    </div>
+    <div class="pro_container">{{title}}</div>
   </div>
 </template>
 
 <script>
-import Vue from "vue";
+import Vue from 'vue'
 import Component from 'vue-class-component'
 
 @Component
 export default class Home extends Vue {
-
-
+  title = '你好a'
 }
 </script>
 
 <style lang="scss" scoped>
-
 </style>
